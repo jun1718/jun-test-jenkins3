a435f12ce4b185223a50bd4ea756241f981e0dc5
jun1718 <gbeovhsqhtka@naver.com>
2022-12-22 17:22:23 +0900

    d

--- src/main/java/com/example/juntestjenkins3/JunTestJenkins3Application.java
+++ src/main/java/com/example/juntestjenkins3/JunTestJenkins3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JunTestJenkins3Application {
 
     public static void main(String[] args) {
+        System.out.println("hi");
         SpringApplication.run(JunTestJenkins3Application.class, args);
     }
 
